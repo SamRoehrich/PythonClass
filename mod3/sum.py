44fc4cee94d54e4896afbcf4f85146b2075cdac1
SamRoehrich <sam_roehrich@icloud.com>
2020-08-31 22:09:55 -0700

    adding sum.py because I am not going to modify the provided file, getting everything out of this course

---
+++ mod3/sum.py
@@ -0,0 +1,10 @@
+# take input from the user
+# while the input is still a number continue accepting input
+# If they press enter quit the loop and
+# calculate the sum and average of the numbers entered
+
+
+def getInput(input, prevInputs):
+
+    nextNumber = int(
+        input("Enter the next nunber or press Enter to exit the loop"))
